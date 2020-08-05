f830a6b4e0a28a1580d8bbabba6a063af7803e3e
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-08-05 13:53:36 -0700

    Added nav color and make buttons into text

--- components/nav.js
+++ components/nav.js
@@ -13,11 +13,11 @@ const links = [
 export default function Nav() {
   return (
     <nav>
-      <ul className="flex justify-between items-center p-6 bg-gray-200">
+      <ul className="flex justify-between items-center p-4 px-12 bg-blue-900">
         <li>
           <Link href="/">
-            <a className="text-lg font-semibold text-blue-700 no-underline">
-              SimiStore
+            <a className="text-lg font-semibold text-white no-underline">
+              simistore
             </a>
           </Link>
         </li>
@@ -25,8 +25,7 @@ export default function Nav() {
           {links.map(({ href, label, icon }) => (
             <li key={`${href}${label}${icon}`}>
               <Link href={href}>
-                <a className="bg-transparent hover:bg-blue-700 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-700 hover:border-transparent rounded">
-                  {/* <img width="8" src={icon}></img> */}
+                <a className="text-gray-100 font-semibold hover:underline py-2 px-4">
                   {label}
                 </a>
               </Link>
