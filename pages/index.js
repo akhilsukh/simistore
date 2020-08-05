2a994a4e076288bc17d1f4a93ab1258313c46f4b
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-08-05 13:53:19 -0700

    Changed website name

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ export default function IndexPage() {
   return (
     <div>
       <Head>
-        <title>SimiCraft</title>
+        <title>simistore</title>
       </Head>
       <Nav />
       <div className="py-20">
         <h1 className="text-5xl text-center text-accent-1">
-          Welcome to <b>simicraft.com</b>
+          Welcome to <b className="text-blue-900">simistore.com</b>
         </h1>
       </div>
     </div>
