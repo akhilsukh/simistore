26f7d4c6602229278d4d7f1b1c7ef6dc861e8ff8
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-08-05 20:33:27 -0700

    Added card-container to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Nav from "../components/nav";
+import Card from "../components/card";
 import Head from "next/head";
+
+import mask1 from "../images/mask1.png"
 
 export default function IndexPage() {
   return (
@@ -10,8 +13,14 @@ export default function IndexPage() {
       <Nav />
       <div className="py-20">
         <h1 className="text-5xl text-center text-accent-1">
-          Welcome to <b className="text-blue-900">simistore.com</b>
+          Welcome to <b className="text-blue-700">simistore.com</b>
         </h1>
+      </div>
+      <div className="card-container">
+        <Card name="Yellow Mask" price="14.99" img={require("../images/mask1.png")} />
+        <Card name="Blue Scarf" price="9.99" img={require("../images/shirt1.png")} />
+        <Card name="Polka Dot Mask" price="14.99" img={require("../images/mask2.png")} />
+        <Card name="Cotton Mask" price="14.99" img={require("../images/mask1.png")} />
       </div>
     </div>
   );
