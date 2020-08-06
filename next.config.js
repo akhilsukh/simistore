26303a0538970767d51fc84c7e39a640c58e0084
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-08-05 20:55:38 -0700

    Update libraries

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withImages = require('next-images')
-module.exports = withImages()
-// module.exports = {
-//     distDir: 'build',
-//     target: 'serverless',
-// }
+// module.exports = withImages()
+module.exports = {
+    distDir: 'build',
+    target: 'serverless',
+}
+module.exports = withImages();
