2bc109aa6ed7f401ffa1114d8a52fdae2a7e4a60
Akhil Sukhthankar <akhilsukh01@gmail.com>
2022-01-16 23:42:26 -0800

    Add visit count and update some dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import '../styles/index.css'
+import React, { useEffect, useState } from "react";
+import countapi from "countapi-js";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const key = "8125a197-987b-428f-921c-78b32f9e3c03"
+  const [visits, setVisits] = useState(0);
+
+  useEffect(() => {
+    getVisits();
+  }, []);
+
+  const getVisits = async () => {
+    try {
+      // countapi.set(key, 0)
+      //     .then((result) => {
+      //       setVisits(result.value)
+      //     });
+      countapi.visits(key)
+          .then((result) => {
+            setVisits(result.value)
+          });
+    } catch (err) {
+      console.log(err.message)
+    }
+  };
+  return <div>
+    <Component {...pageProps} />
+    <div className="py-8 mx-auto">
+      <p className={"text-center"}>
+        Visits: {visits}
+      </p>
+    </div>
+  </div>
 }
 
 export default MyApp
